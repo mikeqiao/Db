e917209d2c0eaa05ec1f6b798701cb516639405a
mikeqiao <374568493@qq.com>
2020-05-28 21:21:07 +0800

    修改代码

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/mikeqiao/Db
+
+go 1.14
+
+require (
+	github.com/gomodule/redigo v1.8.1
+	github.com/mikeqiao/Db v0.0.0-20200528065113-61fbf568b6f7
+)
---
+++ redis/string.go
@@ -0,0 +1,10 @@
+package redis
+
+func (r *CRedis) DelTable(table string) {
+	c := r.Pool.Get()
+	_, err := c.Do("del", table)
+	if nil != err {
+		log.Error("table:%v, error:%v", table, err)
+	}
+	c.Close()
+}
