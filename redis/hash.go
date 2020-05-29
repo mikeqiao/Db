625d9842d25c14a8b033d9ff2401721175b89604
mikeqiao <374568493@qq.com>
2020-05-29 14:56:47 +0800

    添加redis  方法

--- redis/hash.go
+++ redis/hash.go
@@ -1,1 +1,16 @@
 package redis
+
+import (
+	"github.com/gomodule/redigo/redis"
+)
+
+func (r *CRedis) Hash_GetAllData(table string) (map[string]string, error) {
+	c := r.Pool.Get()
+	value, err := redis.StringMap(c.Do("hgetall", table))
+	if nil != err {
+		//	log.Error("table:%v, error:%v", table, err)
+	}
+
+	c.Close()
+	return value, err
+}
--- redis/string.go
+++ redis/string.go
@@ -4,7 +4,7 @@ func (r *CRedis) DelTable(table string) {
 	c := r.Pool.Get()
 	_, err := c.Do("del", table)
 	if nil != err {
-		log.Error("table:%v, error:%v", table, err)
+		//	log.Error("table:%v, error:%v", table, err)
 	}
 	c.Close()
 }
